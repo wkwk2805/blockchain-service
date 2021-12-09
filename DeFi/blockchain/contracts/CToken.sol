5eebebb4eb2cb04f75b1165448749d3f9509b85e
wkwk2 <wkwk2805@gmail.com>
2021-12-10 01:17:54 +0900

    Token, uniswap deploy

---
+++ DeFi/blockchain/contracts/CToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CToken is ERC20{
+
+    constructor() ERC20("CToken", "BTK") {}
+    
+    function mint (address account, uint256 amount )  public {
+        _mint(account, amount);
+    }
+
+    function burn (address account, uint256 amount )  public {
+        _burn(account, amount);
+    }
+}
