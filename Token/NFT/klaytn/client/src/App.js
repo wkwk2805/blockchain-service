34d8fd07708136cbc037623fea48bdf1114e3d03
wkwk2 <wkwk2805@gmail.com>
2022-02-07 23:26:53 +0900

    재정리 unbox react 활용

---
+++ Token/NFT/klaytn/client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Good to Go!</h1>
+      <p>Your Truffle Box is installed and ready.</p>
+      <h2>Smart Contract Example</h2>
+      <p>
+        If your contracts compiled and migrated successfully, below will show a
+        stored value of 5 (by default).
+      </p>
+      <p>
+        Try changing the value stored on <strong>line 42</strong> of App.js.
+      </p>
+    </div>
+  );
+};
+
+export default App;
